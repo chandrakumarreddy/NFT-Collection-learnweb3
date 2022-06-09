e3056928d47702ad979daa3a1fb6437e1f0b182a
chandrakumarreddy <chandrapenugonda655@gmail.com>
2022-06-09 13:02:37 +0530

    update meta url

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,4 +1,4 @@
 const WHITELIST_CONTRACT_ADDRESS = "0x9277B365D6b170f62D951CBEDb91A9b07F8F4E4B";
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://nft-collection-learnweb3-nine.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
